755328f5771ae510773f662c6e2638d391449f9f
Neroznak <dxnz@bk.ru>
2024-12-19 14:44:05 +0100

    Add db and resourses

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { CollabModule } from './collab/collab.module';
+import { CourseModule } from './course/course.module';
+import { LessonModule } from './lesson/lesson.module';
+import { MessageModule } from './message/message.module';
+import { TaskModule } from './task/task.module';
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule]
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, CollabModule, CourseModule, LessonModule, MessageModule, TaskModule]
 })
 export class AppModule {}
